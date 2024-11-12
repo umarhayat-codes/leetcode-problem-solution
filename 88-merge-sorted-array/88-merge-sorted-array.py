2ed3b36de3edae9f6df89a464556eb3b196125e0
umar33102 <uh44171@gmail.com>
2024-11-12 09:51:03 +0500

    merge with easy approach

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,6 +1,5 @@
-nums1 = [0]
-nums2 = [1]
-# nums1[:] = nums1[:len(nums1) - len(nums2)] + nums2
-# print(sorted(nums1))
-
-print(nums1[0:-1])
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+nums1[:] = nums1[:len(nums1) - len(nums2)] + nums2
+print(nums1.sort())
+        
