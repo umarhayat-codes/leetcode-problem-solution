e8ce2d1fcaab1d5936d3afbaf0b8e083f28a7422
Umar Hayat <umarhayat.coder@gmail.com>
2024-11-17 16:28:52 +0500

    Time: 3 ms (44.26%) | Memory: 22.09 MB (18.19%) - LeetSync

---
+++ 344-reverse-string/344-reverse-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseString(self,s):
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return s
+obj = Solution()
+print(obj.reverseString(['a','b','c','d','f','e']))
+
+
+
+
+
