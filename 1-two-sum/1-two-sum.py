ce7c4ab74b0c25060737ad3b952bbe413bbb58a0
Umar Hayat <umarhayat.coder@gmail.com>
2024-11-22 15:04:25 +0500

    Time: 1754 ms (9.34%) | Memory: 17.4 MB (91.00%) - LeetSync

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,9 @@
+nums = [2,7,11,15]
+target = 9
+
+arr = []
+for i in range(len(nums) - 1):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            arr.append([i,j])
+print(arr)
