9f0b50f3d5b8880ec3ae8199c19504ef44038f3b
umar hayat <umarhayat.coder@gmail.com>
2024-11-26 17:03:44 +0500

    Time: 11 ms (24.25%) | Memory: 18.46 MB (15.34%) - LeetSync

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -1,10 +1,29 @@
+# class Solution:
+#     def moveZeroes(self,nums):
+#         l = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[l], nums[i] = nums[i], nums[l]
+#                 l += 1
+#         print(nums)
+# obj = Solution()
+# obj.moveZeroes([0,1,0,3,12]) 
+
+
 class Solution:
     def moveZeroes(self,nums):
-        l = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[l], nums[i] = nums[i], nums[l]
-                l += 1
-        print(nums)
+        left = 0
+        for right in range(1,len(nums)):
+            if nums[left] != 0:
+                left += 1 
+                continue
+            if nums[right] != 0:
+                nums[left] ,nums[right] = nums[right] ,nums[left]
+                left += 1
+        return nums
 obj = Solution()
-obj.moveZeroes([0,1,0,3,12]) 
+print(obj.moveZeroes([0,1,0,3,12]))
+
+
+
+
